0a82a39ab3ca15f8c81b59f98fe97157a2c4e9d2
3yeskxng <zieglerkimi57@gmail.com>
2026-06-21 18:39:03 +0200

    v.0.0.3
    Auto start app.js with Tauri

--- elysium-ui/src-tauri/src/lib.rs
+++ elysium-ui/src-tauri/src/lib.rs
@@ -1,3 +1,6 @@
+use tauri::Manager;
+use std::process::Command;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -9,6 +12,24 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        // HIER WIRD DAS BACKEND GESTARTET
+        .setup(|app| {
+            // 1. Holt sich den Pfad, wo Tauri die Ressourcen (deinen backend-Ordner) installiert hat
+            let resource_path = app.path().resource_dir()
+                .expect("Fehler beim Laden des Ressourcen-Verzeichnisses")
+                .join("backend");
+
+            // 2. Setzt den genauen Pfad zur app.js zusammen
+            let backend_entry = resource_path.join("app.js");
+
+            // 3. Startet 'node backend/app.js' unsichtbar im Hintergrund
+            Command::new("node")
+                .arg(backend_entry)
+                .spawn()
+                .expect("Node-Backend konnte nicht gestartet werden");
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
